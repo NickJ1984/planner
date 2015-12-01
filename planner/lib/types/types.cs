1bf8bf137f6fd79bf2f12e5576aac078b1e26d9e
NickJ1984 <nikolay.yudaev@gmail.com>
2015-12-01 14:30:59 +0300

    dot_Limit complete

--- planner/lib/types/types.cs
+++ planner/lib/types/types.cs
@@ -8,11 +8,11 @@ namespace lib.types
 { 
 
     public enum e_dot_aspect { Dot = 0, LeftDot = 1, RightDot = 2 }
-    public enum e_dot_Limit { None = 0, notEarlier = 4, notLater = 8, inDate = 16 }
+    public enum e_dot_Limit { None = 0, notEarlier = 4, notLater = 8, inDate = 16, Range = 32 }
     [Flags]
     public enum e_dot_stateLimit { Free = 0, LimitLeft = 1, LimitRight = 2, notEarlier = 4, notLater = 8 }
     [Flags]
-    public enum e_dot_Fixed {Free = 0, FixedLeft = 32, FixedRight = 64, Fixed = 128, manualFixed = 256 }
+    public enum e_dot_Fixed {Free = 0, FixedLeft = 64, FixedRight = 128, Fixed = 256}
     public enum e_dot_stateValue { Null = 0, Source = 1, Declared = 2, Changed = 3}
     public enum e_dot_sender { This = 0, Val_Declare = 1, Val_Source = 2, Val_Fixed = 3}
     public enum e_ValueType { Null = 0, Dot = 1, Period = 2}
